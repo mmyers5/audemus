989419e3e4df76c0168bff2e074f28c7c3d9d2ac
Michelle Myers <mmyers5@users.noreply.github.com>
2020-02-25 23:31:24 -0800

    Add pc and remove deprecated routes

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -1,134 +1,29 @@
 from flask import Flask, request, render_template
 
-from apps.bran import scrape, doc_write
-from apps.brawler import brawl
-from apps.jeeves import scrape, score
-from apps.whippet import get_one
-
-import utils
+from apps.pc.jenny import PcSchema
 
 app = Flask(__name__)
 
-@app.route('/whippet/', methods=['GET', 'POST'])
-def whippet():
-    template = 'whippet.html'
-    values = get_one.get_sheet_values()
-    df = get_one.sheet_values_to_df(values)
-    locations = sorted(df.location.unique())
-    if request.method == 'GET':
-        form = {
-            'location': locations[0],
-            'printout': ''
-        }
-    elif request.method == 'POST':
-        location = request.form['location']
-        location_df = get_one.df_at_location(df, location)
-        location_df = get_one.calc_encounter(location_df)
-        row_entry =  location_df.sample(1, weights='encounter_percent')
-        specie = row_entry.specie.iloc[0]
-        chance = row_entry.encounter_percent.iloc[0]
-        form = {
-            'location': location,
-            'printout': '{specie} - {chance:.2f}%'.format(
-                specie=specie, chance=chance
-            )
-        }
-    return render_template(
-        template,
-        locations=locations,
-        form=form
-    )
 
-@app.route('/bran/', methods=['GET', 'POST'])
-def bran():
-    template = 'bran.html'
+@app.route('/', methods=['GET'])
+def home():
+    return 'Sup, bitch.'
+
+
+@app.route('/pc', methods=['GET', 'POST'])
+def pc_jenny():
     if request.method == 'GET':
-        form = {
-            'username': '',
-            'thread_url': '',
-            'printout': ''
-        }
-    elif request.method == 'POST':
-        username = request.form['username']
-        thread_url = request.form['thread_url']
-        service = doc_write.get_service()
-        thread = scrape.JcinkThread(thread_url)
-        posts, users = thread.ordered_posts()
-        filename = doc_write.filename(
-            username, thread.title, thread.subtitle
+        return render_template(
+            'pc.html',
+            name=None, gender=None, specie_name=None,
+            specie_type=None, ball_name=None, item_name=None,
+            ability_name=None, level=None, bond=None, moves=None,
+            description=None
         )
-        doc = doc_write.write_file(filename, posts, users, service)
-        doc_url = doc_write.doc_url(doc)
+    body = request.get_json()
+    schema = PcSchema()
+    return schema.dump(body)
 
-        form = {
-            'username': username,
-            'thread_url': thread_url,
-            'printout': doc_url
-        }
-    return render_template(
-        template,
-        form=form
-    )
 
-@app.route('/jeeves/', methods=['GET', 'POST'])
-def jeeves():
-    template = 'jeeves.html'
-    if request.method == 'GET':
-        form = {
-            'thread_url': '',
-            'printout': ''
-        }
-    elif request.method == 'POST':
-        thread_url = request.form['thread_url']
-        page = scrape.JcinkPage(thread_url)
-        users = page.users
-        posts = page.posts
-        thread_score = score.ThreadScore(users, posts)
-        printout = thread_score.printout(
-            words_per_level=utils.N_WORDS_LEVEL,
-            words_per_dollar=utils.N_WORDS_DOLLAR
-        )
-        form = {
-            'thread_url': thread_url,
-            'printout': printout
-        }
-    return render_template(
-        template,
-        form=form
-    )
-
-@app.route('/brawler/', methods=['GET', 'POST'])
-def brawler():
-    template = 'brawler.html'
-    if request.method == 'GET':
-        form = {
-            'attacker': '',
-            'attacker_level': '',
-            'defender': '',
-            'defender_level': '',
-            'attack': '',
-            'printout': ''
-        }
-    elif request.method == 'POST':
-        attacker = request.form['attacker']
-        attacker_level = request.form['attacker_level']
-        defender = request.form['defender']
-        defender_level = request.form['defender_level']
-        attack = request.form['attack']
-        b = brawl.Brawl(
-            attacker, attacker_level, attack,
-            defender, defender_level
-        )
-        printout = b.printout
-        form = {
-            'attacker': attacker,
-            'attacker_level': attacker_level,
-            'defender': defender,
-            'defender_level': defender_level,
-            'attack': attack,
-            'printout': printout
-        }
-    return render_template(
-        template,
-        form=form
-    )
+if __name__ == '__main__':
+    app.run()
